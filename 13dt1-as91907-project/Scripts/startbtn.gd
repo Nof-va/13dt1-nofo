6143e2ae7ad666c856da4cf000e80f57a7ef34d8
Nof-va <nvaaiafamasaga@students.mags.school.nz>
2025-06-16 14:40:14 +1200

    Main Menu

---
+++ 13dt1-as91907-project/Scripts/startbtn.gd
@@ -0,0 +1,16 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+#Called when button is pressed
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/gameplay.tscn") # Transports user from current scene to gameplay scnene
