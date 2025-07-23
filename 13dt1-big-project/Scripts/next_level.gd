dc7546c38da69f2e19943ffe1545f42d9eb774f9
Nof-va <nvaaiafamasaga@students.mags.school.nz>
2025-07-23 12:29:24 +1200

    Fix 2

---
+++ 13dt1-big-project/Scripts/next_level.gd
@@ -0,0 +1,17 @@
+extends Area3D
+
+var next_level = preload("res://Scenes/gameplay_2.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("Player"):
+		get_tree().change_scene_to_packed.bind(next_level).call_deferred()
