421124ce80473a0f109a67c40fd11f2861e48035
Nof-va <nvaaiafamasaga@students.mags.school.nz>
2025-10-06 14:48:16 +1300

    Door refined + key slot

--- 13dt1-big-project/Scripts/door.gd
+++ 13dt1-big-project/Scripts/door.gd
@@ -1,12 +1,24 @@
-extends Area3D
+extends Node3D
 
-@export var animator : AnimatableBody3D
+@export var raycast : RayCast3D
+@export var door_animator : AnimatableBody3D
+@export var key_place : Marker3D
+var door_open = false
+var collider
 
-func _on_body_entered(body: Node3D) -> void:
-	if body.is_in_group("Moveable"):
-		animator.open()
 
-
-func _on_body_exited(body: Node3D) -> void:
-	if body.is_in_group("Moveable"):
-		animator.close()
+func _physics_process(_delta: float) -> void:
+	
+	if raycast.is_colliding():
+		collider = raycast.get_collider()
+		
+		if collider.is_in_group("Moveable"):
+			collider.position = key_place.global_position
+			if door_open == false:
+				door_animator.open()
+				door_open = true
+		
+	elif door_open == true:
+			door_animator.close()
+			door_open = false
+	
