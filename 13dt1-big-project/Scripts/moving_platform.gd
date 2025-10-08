2f223419393f83674dc9a1e430cbe82a0e6ed816
Nof-va <nvaaiafamasaga@students.mags.school.nz>
2025-10-09 10:09:29 +1300

    Code and level fixes

--- 13dt1-big-project/Scripts/moving_platform.gd
+++ 13dt1-big-project/Scripts/moving_platform.gd
@@ -2,7 +2,7 @@ extends AnimatableBody3D
 
 @export var a := Vector3()
 @export var b := Vector3()
-@export var time : float = 5.0
+@export var time : float = 2.0
 @export var pause : float = 1
 
 func _ready() -> void:
@@ -13,5 +13,5 @@ func move():
 	var move_tween = create_tween()
 	move_tween.tween_property(self, "position", b, time).set_trans(Tween.TRANS_SINE).set_delay(pause)
 	move_tween.tween_property(self, "position", a, time).set_trans(Tween.TRANS_SINE).set_delay(pause)
-	await get_tree().create_timer(2 * time + 2 * pause).timeout
+	await move_tween.finished
 	move()
